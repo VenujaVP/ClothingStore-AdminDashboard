5c50f70606582a719e656ba539c52b7b9c1c6470
Venuja Prasanjith <144810283+VenujaVP@users.noreply.github.com>
2025-01-28 23:55:03 +0530

    LoginRegisterUpdate14

--- frontend/customers/src/pages/Dashboard/Dashboard.jsx
+++ frontend/customers/src/pages/Dashboard/Dashboard.jsx
@@ -29,10 +29,10 @@ const Dashboard = () => {
             setMassage(res.data.err)
           }
         })
-        .catch(err => {
-          console.error("Error during authentication:", err);
-        });
 
+        .then(err => {
+          console.log(err);
+        });
   },[])
 
     const handleLogout = () => {
