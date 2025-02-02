e54e694e2e70fdbda8003b2a82c4e9b61e8130a6
Venuja Prasanjith <144810283+VenujaVP@users.noreply.github.com>
2025-02-02 15:05:44 +0530

    Navbar update 02

--- frontend/customers/src/conponent/Navbar/Navbar.jsx
+++ frontend/customers/src/conponent/Navbar/Navbar.jsx
@@ -7,6 +7,7 @@ import './Navbar.css';
 
 const Navbar = () => {
   const [notice, setNotice] = useState('Welcome to our website!');
+  const [activeDropdown, setActiveDropdown] = useState(null); // Track active dropdown
 
   useEffect(() => {
     const notices = [
@@ -25,6 +26,11 @@ const Navbar = () => {
     return () => clearInterval(intervalId); // Cleanup interval on component unmount
   }, []);
 
+  // Function to handle dropdown toggle
+  const toggleDropdown = (category) => {
+    setActiveDropdown(activeDropdown === category ? null : category);
+  };
+
   return (
     <>
       <div className="notice-bar">
@@ -41,13 +47,132 @@ const Navbar = () => {
         </div>
 
         <div className="categories">
-          <div className="category">abv</div>
-          <div className="category">abc</div>
-          <div className="category">abc</div>
-          <div className="category">abc</div>
-          <div className="category">abc</div>
-          <div className="category">abc</div>
-          <div className="category">abc</div>
+          {/* HOME */}
+          <div className="category">HOME</div>
+
+          {/* WOMEN */}
+          <div
+            className="category"
+            onMouseEnter={() => toggleDropdown('WOMEN')}
+            onMouseLeave={() => toggleDropdown(null)}
+          >
+            WOMEN
+            {activeDropdown === 'WOMEN' && (
+              <div className="dropdown">
+                <div className="dropdown-item">New Arrivals</div>
+                <div className="dropdown-item">
+                  Tops & Tees
+                  <div className="sub-dropdown">
+                    <div className="sub-dropdown-item">Blouses</div>
+                    <div className="sub-dropdown-item">Crop Tops</div>
+                    <div className="sub-dropdown-item">T-Shirts</div>
+                    <div className="sub-dropdown-item">Hoodies & Sweaters</div>
+                  </div>
+                </div>
+                <div className="dropdown-item">
+                  Dresses & Bottoms
+                  <div className="sub-dropdown">
+                    <div className="sub-dropdown-item">Dresses & Frocks</div>
+                    <div className="sub-dropdown-item">Skirts</div>
+                    <div className="sub-dropdown-item">Trousers</div>
+                    <div className="sub-dropdown-item">Denims</div>
+                    <div className="sub-dropdown-item">Shorts</div>
+                    <div className="sub-dropdown-item">Pants</div>
+                  </div>
+                </div>
+                <div className="dropdown-item">
+                  Special Categories
+                  <div className="sub-dropdown">
+                    <div className="sub-dropdown-item">Jumpsuits</div>
+                    <div className="sub-dropdown-item">Bodysuits</div>
+                    <div className="sub-dropdown-item">Office Wear</div>
+                    <div className="sub-dropdown-item">Gym Wear</div>
+                    <div className="sub-dropdown-item">Night & Loungewear</div>
+                  </div>
+                </div>
+              </div>
+            )}
+          </div>
+
+          {/* MEN */}
+          <div
+            className="category"
+            onMouseEnter={() => toggleDropdown('MEN')}
+            onMouseLeave={() => toggleDropdown(null)}
+          >
+            MEN
+            {activeDropdown === 'MEN' && (
+              <div className="dropdown">
+                <div className="dropdown-item">New Arrivals</div>
+                <div className="dropdown-item">
+                  Tops
+                  <div className="sub-dropdown">
+                    <div className="sub-dropdown-item">Shirts</div>
+                    <div className="sub-dropdown-item">T-Shirts</div>
+                    <div className="sub-dropdown-item">Hoodies & Sweaters</div>
+                  </div>
+                </div>
+                <div className="dropdown-item">
+                  Bottoms
+                  <div className="sub-dropdown">
+                    <div className="sub-dropdown-item">Trousers</div>
+                    <div className="sub-dropdown-item">Denims</div>
+                    <div className="sub-dropdown-item">Shorts</div>
+                    <div className="sub-dropdown-item">Pants</div>
+                  </div>
+                </div>
+                <div className="dropdown-item">
+                  Special Categories
+                  <div className="sub-dropdown">
+                    <div className="sub-dropdown-item">Office Wear</div>
+                    <div className="sub-dropdown-item">Gym Wear</div>
+                  </div>
+                </div>
+              </div>
+            )}
+          </div>
+
+          {/* KIDS & BABY */}
+          <div
+            className="category"
+            onMouseEnter={() => toggleDropdown('KIDS & BABY')}
+            onMouseLeave={() => toggleDropdown(null)}
+          >
+            KIDS & BABY
+            {activeDropdown === 'KIDS & BABY' && (
+              <div className="dropdown">
+                <div className="dropdown-item">New Arrivals</div>
+                <div className="dropdown-item">Boys&apos; Clothing (3-16)</div>
+                <div className="dropdown-item">Girls&apos; Clothing (3-16)</div>
+                <div className="dropdown-item">Baby Clothing</div>
+                <div className="dropdown-item">Footwear</div>
+                <div className="dropdown-item">
+                  Bags & Accessories
+                  <div className="sub-dropdown">
+                    <div className="sub-dropdown-item">Kids&apos; Bags</div>
+                    <div className="sub-dropdown-item">Kids&apos; Watches</div>
+                    <div className="sub-dropdown-item">Hats & Caps</div>
+                  </div>
+                </div>
+                <div className="dropdown-item">Mother & Baby Care</div>
+              </div>
+            )}
+          </div>
+
+          {/* WATCHES */}
+          <div className="category">WATCHES</div>
+
+          {/* FOOTWEAR */}
+          <div className="category">FOOTWEAR</div>
+
+          {/* BAGS & WALLETS */}
+          <div className="category">BAGS & WALLETS</div>
+
+          {/* ACCESSORIES */}
+          <div className="category">ACCESSORIES</div>
+
+          {/* SALE & OFFERS */}
+          <div className="category">SALE & OFFERS</div>
         </div>
 
         <div className="icons">
