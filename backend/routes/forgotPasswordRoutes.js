bc079e6959ff5506464b11484e5f333cd02236a7
Venuja Prasanjith <144810283+VenujaVP@users.noreply.github.com>
2025-02-07 15:42:18 +0530

    Forget Password Implmantation 02

---
+++ backend/routes/forgotPasswordRoutes.js
@@ -0,0 +1,11 @@
+//routes/forgotPasswordRoutes.js
+
+import express from 'express';
+import { requestPasswordReset, resetPassword } from '../controllers/forgotPasswordController.js';
+
+const router = express.Router();
+
+router.post('/request-reset', requestPasswordReset);  // Step 1: Send reset email
+router.post('/reset', resetPassword);  // Step 2: Reset password
+
+export default router;
