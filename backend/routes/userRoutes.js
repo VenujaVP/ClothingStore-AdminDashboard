8cba3e672a4574f6cca04f4d068cb361cb902925
Venuja Prasanjith <144810283+VenujaVP@users.noreply.github.com>
2025-03-21 17:11:20 +0530

    update 35

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,11 @@
 //routes/userRoutes.js
 
 import express from 'express';
-import { fetchProducts } from '../controllers/userControllers.js';
+import { fetchProducts, searchProducts } from '../controllers/userControllers.js';
 
 const router = express.Router();
 
 router.get('/product-fetch', fetchProducts);
+router.post('/product-search', searchProducts);
 
 export default router;
