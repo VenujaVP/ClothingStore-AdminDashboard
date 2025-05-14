4ab5a6795841f936b55a7529b4600537ee29cc3d
Venuja Prasanjith <144810283+VenujaVP@users.noreply.github.com>
2025-05-14 10:52:40 +0530

    feat: log product variations after fetching product details

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -294,7 +294,7 @@ import sqldb from '../config/sqldb.js';
                       };
   
                       res.json(responseData);
-                      // console.log('Product details fetched successfully:', responseData);
+                      console.log('Product details fetched successfully:', responseData.product.variations);
                   }
               );
           }
